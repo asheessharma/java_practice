f6d5d50f62351ab130113e08ab8246837b94ee0d
asheessharma <ashees.sharma_cs20@gla.ac.in>
2021-10-02 00:20:51 +0530

    commit

---
+++ src/passing_array.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class passing_array {
+    void method1(int [] arr1,int m,int val1){
+        arr1[m]=val1;
+        System.out.println("New array is :-");
+       for (int i=0;i<arr1.length;i++){
+           System.out.println(arr1[i]+" ");
+       }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of array");
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        System.out.println(" Enter "+n+" values of your choice:-");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int j,val;
+        System.out.println("Enter index number:-");
+        j=sc.nextInt();
+        System.out.println(" Enter  value of index:-");
+        val=sc.nextInt();
+        System.out.println("initially your array is:-");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println( );
+         passing_array pass=new passing_array();
+        pass.method1(arr,j,val);
+
+    }
+}
